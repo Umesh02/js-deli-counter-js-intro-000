f980e3f28da00358965496fff6b952e901719a34
Umesh K <u.teach.and.i.learn@gmail.com>
2019-05-01 06:59:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ function takeANumber(katzDeliLine, customerName) {
   katzDeliLine.push(customerName)
   return `Welcome, ${customerName}. You are number ${katzDeliLine.indexOf(customerName) + 1} in line.`
 }
-
