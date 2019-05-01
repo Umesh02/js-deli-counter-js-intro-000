da86fbe75e3f5f7a29105dd6970f4231af6fed19
Umesh K <u.teach.and.i.learn@gmail.com>
2019-05-01 07:23:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeliLine) {
 if (katzDeliLine.length === 0) {
   return `There is nobody waiting to be served!`
 } else {
-  personBeingServed = katzDeliLine[0]
+  var personBeingServed = katzDeliLine[0]
   katzDeliLine.splice(0, 1)
   return personBeingServed
 }
