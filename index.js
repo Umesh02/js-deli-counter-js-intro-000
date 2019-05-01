5987ba545dd65cd3954f563a5f4faccaf79696ce
Umesh K <u.teach.and.i.learn@gmail.com>
2019-05-01 07:05:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function takeANumber(katzDeliLine, customerName) {
   katzDeliLine.push(customerName)
   return `Welcome, ${customerName}. You are number ${katzDeliLine.indexOf(customerName) + 1} in line.`
 }
+
+function nowServing(katzDeliLine) {
+if (katzDeliLine.length === 0) {
+  return `There is nobody waiting to be served!`
+} else {
+  personBeingServed = katzDeliLine[0]
+  katzDeliLine.splice(0, 1)
+  return personBeingServed
+}
+  
+}
